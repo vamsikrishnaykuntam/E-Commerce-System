32a6b4f06d57d9fc3a741f0a08e90d0b3ebb7af9
Vamsi Krishna Ykuntam <vk.ykuntam@gmail.com>
2018-09-09 21:17:20 +0530

    E Commerce system

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="container">
+        <center>
+            <p>Copyright &copy; Lifestyle Store. All Rights Reserved @ Vamsi Krishna |  Contact Us: +91 7799670710</p>	
+        </center>
+    </div>
+</footer>
